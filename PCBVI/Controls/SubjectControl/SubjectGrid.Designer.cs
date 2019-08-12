d649a9319a4650d6e9651e3edeadddeb991c960f
해송 최 <rbghf@naver.com>
2019-08-12 17:32:16 +0900

    2019.08.12.SubjectForm

--- PCBVI/Controls/SubjectControl/SubjectGrid.Designer.cs
+++ PCBVI/Controls/SubjectControl/SubjectGrid.Designer.cs
@@ -28,19 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.SubjectGridView = new ComponentFactory.Krypton.Toolkit.KryptonDataGridView();
+            this.productBds = new System.Windows.Forms.BindingSource(this.components);
+            this.productIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.baecodeIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.productSubjectIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.workPlaceIdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DetailContents = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.SubjectGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBds)).BeginInit();
             this.SuspendLayout();
             // 
             // SubjectGridView
             // 
+            this.SubjectGridView.AutoGenerateColumns = false;
             this.SubjectGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SubjectGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.productIdDataGridViewTextBoxColumn,
+            this.productCodeDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.baecodeIdDataGridViewTextBoxColumn,
+            this.productSubjectIdDataGridViewTextBoxColumn,
+            this.workPlaceIdDataGridViewTextBoxColumn,
+            this.quantityDataGridViewTextBoxColumn,
+            this.DetailContents});
+            this.SubjectGridView.DataSource = this.productBds;
             this.SubjectGridView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.SubjectGridView.Location = new System.Drawing.Point(0, 0);
             this.SubjectGridView.Name = "SubjectGridView";
             this.SubjectGridView.RowTemplate.Height = 23;
             this.SubjectGridView.Size = new System.Drawing.Size(800, 150);
             this.SubjectGridView.TabIndex = 0;
+            // 
+            // productBds
+            // 
+            this.productBds.DataSource = typeof(PCBVI.Data.Product);
+            // 
+            // productIdDataGridViewTextBoxColumn
+            // 
+            this.productIdDataGridViewTextBoxColumn.DataPropertyName = "ProductId";
+            this.productIdDataGridViewTextBoxColumn.HeaderText = "ProductID";
+            this.productIdDataGridViewTextBoxColumn.Name = "productIdDataGridViewTextBoxColumn";
+            // 
+            // productCodeDataGridViewTextBoxColumn
+            // 
+            this.productCodeDataGridViewTextBoxColumn.DataPropertyName = "ProductCode";
+            this.productCodeDataGridViewTextBoxColumn.HeaderText = "ProductCode";
+            this.productCodeDataGridViewTextBoxColumn.Name = "productCodeDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // baecodeIdDataGridViewTextBoxColumn
+            // 
+            this.baecodeIdDataGridViewTextBoxColumn.DataPropertyName = "BaecodeId";
+            this.baecodeIdDataGridViewTextBoxColumn.HeaderText = "BaecodeId";
+            this.baecodeIdDataGridViewTextBoxColumn.Name = "baecodeIdDataGridViewTextBoxColumn";
+            // 
+            // productSubjectIdDataGridViewTextBoxColumn
+            // 
+            this.productSubjectIdDataGridViewTextBoxColumn.DataPropertyName = "ProductSubjectId";
+            this.productSubjectIdDataGridViewTextBoxColumn.HeaderText = "ProductSubjectId";
+            this.productSubjectIdDataGridViewTextBoxColumn.Name = "productSubjectIdDataGridViewTextBoxColumn";
+            // 
+            // workPlaceIdDataGridViewTextBoxColumn
+            // 
+            this.workPlaceIdDataGridViewTextBoxColumn.DataPropertyName = "WorkPlaceId";
+            this.workPlaceIdDataGridViewTextBoxColumn.HeaderText = "WorkPlaceId";
+            this.workPlaceIdDataGridViewTextBoxColumn.Name = "workPlaceIdDataGridViewTextBoxColumn";
+            // 
+            // quantityDataGridViewTextBoxColumn
+            // 
+            this.quantityDataGridViewTextBoxColumn.DataPropertyName = "Quantity";
+            this.quantityDataGridViewTextBoxColumn.HeaderText = "Quantity";
+            this.quantityDataGridViewTextBoxColumn.Name = "quantityDataGridViewTextBoxColumn";
+            // 
+            // DetailContents
+            // 
+            this.DetailContents.DataPropertyName = "DetailContents";
+            this.DetailContents.HeaderText = "DetailContents";
+            this.DetailContents.Name = "DetailContents";
             // 
             // SubjectGrid
             // 
@@ -50,6 +124,7 @@
             this.Name = "SubjectGrid";
             this.Size = new System.Drawing.Size(800, 150);
             ((System.ComponentModel.ISupportInitialize)(this.SubjectGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productBds)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +132,14 @@
         #endregion
 
         private ComponentFactory.Krypton.Toolkit.KryptonDataGridView SubjectGridView;
+        private System.Windows.Forms.BindingSource productBds;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productCodeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn baecodeIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn productSubjectIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workPlaceIdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DetailContents;
     }
 }
--- PCBVI/Forms/Basic/SubJectForm.Designer.cs
+++ PCBVI/Forms/Basic/SubJectForm.Designer.cs
@@ -28,43 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.subjectSearch1 = new PCBVI.Controls.SubjectControl.SubjectSearch();
-            this.topMenubar1 = new PCBVI.Controls.CommonControl.TopMenubar();
             this.kryptonNavigator1 = new ComponentFactory.Krypton.Navigator.KryptonNavigator();
             this.kryptonPage1 = new ComponentFactory.Krypton.Navigator.KryptonPage();
             this.subjectGrid1 = new PCBVI.Controls.SubjectControl.SubjectGrid();
+            this.subjectSearch1 = new PCBVI.Controls.SubjectControl.SubjectSearch();
+            this.topMenubar1 = new PCBVI.Controls.CommonControl.TopMenubar();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonNavigator1)).BeginInit();
             this.kryptonNavigator1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonPage1)).BeginInit();
             this.kryptonPage1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // subjectSearch1
-            // 
-            this.subjectSearch1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.subjectSearch1.Location = new System.Drawing.Point(0, 0);
-            this.subjectSearch1.Name = "subjectSearch1";
-            this.subjectSearch1.Size = new System.Drawing.Size(798, 75);
-            this.subjectSearch1.TabIndex = 0;
-            // 
-            // topMenubar1
-            // 
-            this.topMenubar1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.topMenubar1.Location = new System.Drawing.Point(0, 0);
-            this.topMenubar1.Name = "topMenubar1";
-            this.topMenubar1.Size = new System.Drawing.Size(800, 35);
-            this.topMenubar1.TabIndex = 1;
-            // 
             // kryptonNavigator1
             // 
             this.kryptonNavigator1.Button.ButtonDisplayLogic = ComponentFactory.Krypton.Navigator.ButtonDisplayLogic.None;
             this.kryptonNavigator1.Button.CloseButtonAction = ComponentFactory.Krypton.Navigator.CloseButtonAction.None;
             this.kryptonNavigator1.Button.CloseButtonDisplay = ComponentFactory.Krypton.Navigator.ButtonDisplay.Hide;
+            this.kryptonNavigator1.Button.ContextButtonAction = ComponentFactory.Krypton.Navigator.ContextButtonAction.SelectPage;
+            this.kryptonNavigator1.Button.ContextButtonDisplay = ComponentFactory.Krypton.Navigator.ButtonDisplay.Logic;
+            this.kryptonNavigator1.Button.ContextMenuMapImage = ComponentFactory.Krypton.Navigator.MapKryptonPageImage.Small;
+            this.kryptonNavigator1.Button.ContextMenuMapText = ComponentFactory.Krypton.Navigator.MapKryptonPageText.TextTitle;
+            this.kryptonNavigator1.Button.NextButtonAction = ComponentFactory.Krypton.Navigator.DirectionButtonAction.ModeAppropriateAction;
+            this.kryptonNavigator1.Button.NextButtonDisplay = ComponentFactory.Krypton.Navigator.ButtonDisplay.Logic;
+            this.kryptonNavigator1.Button.PreviousButtonAction = ComponentFactory.Krypton.Navigator.DirectionButtonAction.ModeAppropriateAction;
+            this.kryptonNavigator1.Button.PreviousButtonDisplay = ComponentFactory.Krypton.Navigator.ButtonDisplay.Logic;
             this.kryptonNavigator1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.kryptonNavigator1.Group.GroupBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.ControlClient;
+            this.kryptonNavigator1.Group.GroupBorderStyle = ComponentFactory.Krypton.Toolkit.PaletteBorderStyle.ControlClient;
+            this.kryptonNavigator1.Header.HeaderStyleBar = ComponentFactory.Krypton.Toolkit.HeaderStyle.Secondary;
+            this.kryptonNavigator1.Header.HeaderStylePrimary = ComponentFactory.Krypton.Toolkit.HeaderStyle.Primary;
+            this.kryptonNavigator1.Header.HeaderStyleSecondary = ComponentFactory.Krypton.Toolkit.HeaderStyle.Secondary;
             this.kryptonNavigator1.Location = new System.Drawing.Point(0, 35);
             this.kryptonNavigator1.Name = "kryptonNavigator1";
+            this.kryptonNavigator1.NavigatorMode = ComponentFactory.Krypton.Navigator.NavigatorMode.BarTabGroup;
+            this.kryptonNavigator1.PageBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.ControlClient;
             this.kryptonNavigator1.Pages.AddRange(new ComponentFactory.Krypton.Navigator.KryptonPage[] {
             this.kryptonPage1});
+            this.kryptonNavigator1.Panel.PanelBackStyle = ComponentFactory.Krypton.Toolkit.PaletteBackStyle.PanelClient;
             this.kryptonNavigator1.SelectedIndex = 0;
             this.kryptonNavigator1.Size = new System.Drawing.Size(800, 415);
             this.kryptonNavigator1.TabIndex = 2;
@@ -81,15 +81,34 @@
             this.kryptonPage1.Name = "kryptonPage1";
             this.kryptonPage1.Size = new System.Drawing.Size(798, 388);
             this.kryptonPage1.Text = "품목정보";
+            this.kryptonPage1.ToolTipStyle = ComponentFactory.Krypton.Toolkit.LabelStyle.ToolTip;
             this.kryptonPage1.ToolTipTitle = "Page ToolTip";
             this.kryptonPage1.UniqueName = "959E674ED33C4409AA829284582CFFF9";
             // 
             // subjectGrid1
             // 
-            this.subjectGrid1.Location = new System.Drawing.Point(-1, 81);
+            this.subjectGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.subjectGrid1.Location = new System.Drawing.Point(0, 75);
             this.subjectGrid1.Name = "subjectGrid1";
-            this.subjectGrid1.Size = new System.Drawing.Size(798, 221);
+            this.subjectGrid1.Size = new System.Drawing.Size(798, 313);
             this.subjectGrid1.TabIndex = 1;
+            // 
+            // subjectSearch1
+            // 
+            this.subjectSearch1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.subjectSearch1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.subjectSearch1.Location = new System.Drawing.Point(0, 0);
+            this.subjectSearch1.Name = "subjectSearch1";
+            this.subjectSearch1.Size = new System.Drawing.Size(798, 75);
+            this.subjectSearch1.TabIndex = 0;
+            // 
+            // topMenubar1
+            // 
+            this.topMenubar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topMenubar1.Location = new System.Drawing.Point(0, 0);
+            this.topMenubar1.Name = "topMenubar1";
+            this.topMenubar1.Size = new System.Drawing.Size(800, 35);
+            this.topMenubar1.TabIndex = 1;
             // 
             // SubJectForm
             // 
